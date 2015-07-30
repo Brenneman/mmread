f41c95138a03a7a970fe6abc1af107f874e66fe1
Tristan Brenneman <brenneman000@gmail.com>
2015-07-30 12:29:37 -0500

    Added pseudocode

--- mmread.py
+++ mmread.py
@@ -1,12 +1,32 @@
-f = open('conn.txt', 'r+')
+
+#Get and open the MINMAX report file
+def getFile(f):
+    open(f)
+    return(f)
+
+#Here is the old code for reference
+'''f = open('conn.txt', 'r+')
 f.readline()
 f.readline()
 f.readline()
 MMS = f.readline(17)[12:17]
+print(line)
 print("MMS# ", MMS)
 string = str(f.read())
 length = len(string)
 print('There are', length, 'characters in this file!')
-f.close()
+for line in f:
+    print(line)
+f.close()'''
 
 
+#Main run sequence
+
+#Get file name and open file
+Print('File you wish to open?')
+f = input(">>>")
+getFile(f)
+
+#Read file and print list of suppliers found
+
+#Ask whether to output, display items for a supplier, select new file, or exit
