1818c69f71bee203166a6ccec9470f68b04ca4ae
Tristan Brenneman <brenneman000@gmail.com>
2015-08-13 16:14:21 -0500

    Parsing near done...
    Able to parse the first branch which requires the item to a CSV file!
    Exciting times.

--- parse.py
+++ parse.py
@@ -1,73 +1,105 @@
 #This will parse the .txt file and prepare it for writing to csv
-'''
+#Tristan Brenneman
+#St. John's Music
+#August 2015
+
+
+#This opens the file for parsing. Will be removed in later versions.
+#Currently for testing purposes
 def fopen(fname):
     f = open(fname)
-    print("Opened " + fname)
     return(f)
 
+#Finds the line with supplier and item information and adds it to lists for
+#later interpretation
 def getSupp(f):
-    f.seek(0)
+    f.seek(0)   #reset to inital file position
     suppLine = False
     supp = []
+    prod = []
+    itemNum = []
+    descCode = []
+    desc = []
+    
 
     for line in f:
         if suppLine == True:
             supp.append(line[:4])
+            prod.append(line[5:9])
+            itemNum.append(line.lstrip('')[10:17])
+            descCode.append(line.lstrip('')[18:29])
+            desc.append(line.rstrip('\n')[29:])
             suppLine = False
-
+            
         elif line[:4] == 'Supp':
             suppLine = True
 
         else:
             suppLine == False
             
-    return(supp)
+    return(supp, prod, itemNum, descCode, desc)
 
-def getItemNum(f, supp):
-    f.seek(0)
-    itemNum = []
-    i = 0
 
-    for line in f:
+#Builds a dictionary for each item that is to be ordered.
+#As of yet it doesn't include Branch or BuyQty. This may change.
+def buildItemDict(itemNum, supp, prod, descCode, desc, br, bQty):
+    itemDict = {}
 
-        if i == len(supp):
-            return(itemNum)
+    while True:
+        itemDict[itemNum.pop().lstrip()] = supp.pop(), prod.pop().rstrip(), descCode.pop().rstrip(), desc.pop().rstrip()
+
+        if len(itemNum) == 0:
             break
 
-        elif line[:4] == supp[i]:
-            itemNum.append(line[9:17])
-            i += 1
-
-        else:
-            pass
-
-def getItemCode(f)
-    f.seek(0)
-    itemCode = []
-
-    for line in f:
+    for key in itemDict:
+        itemDict[key] += br.pop(), bQty.pop()
         
 
-##testcode
+    return(itemDict)
 
-fname = input("File name:\n>>>")
-ifile = fopen(fname)
-isupp = getSupp(ifile)
-iitemNum = getItemNum(ifile, isupp)
-#iitemCode = getItemCode(ifile, i'''
+#Uses the list of item numbers to get the Branch and BuyQty for each item.
+#Needs to be updated/fixed for items with multiple branch buys required.        
+def getBrBqty(itemNums, f):
+    f.seek(0)   #reset to initial file position
+    br = []
+    bqty = []
+    itemNum = []
+    [itemNum.append(x) for x in itemNums if x not in itemNum]
+    i = 0
     
-import struct
+        
+    for line in f:
+        
+        if i == len(itemNum):
+            break
+        
+        inum = itemNum[i]
 
-fieldwidths = (2, 10, 24)  # negative widths represent ignored padding fields
-fmtstring = ' '.join('{}{}'.format(abs(fw), 'x' if fw < 0 else 's')
-                        for fw in fieldwidths)
-fieldstruct = struct.Struct(fmtstring)
-parse = fieldstruct.unpack_from
-print('fmtstring: {!r}, recsize: {} chars'.format(fmtstring, fieldstruct.size))
+        if line[:17].lstrip() == inum.lstrip():
 
-line = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\n'
-fields = parse(line)
-print('fields: {}'.format(fields))
+            br.append(line[29:31])
+            bqty.append(line[63:68].lstrip())
+            i += 1
+
+    return(br, bqty)
+
+        
+        
+
+#Test code for testing the implementations of the above functions.
+#May become a function for the whole file later.
+fname = input(">>> File Name: \n")
+ifile = fopen(fname)
+isupp, iprod, iitemNum, idescCode, idesc = getSupp(ifile)
+ibr, ibQty = getBrBqty(iitemNum, ifile)
+iitemDict = buildItemDict(iitemNum, isupp, iprod, idescCode, idesc, ibr, ibQty)
 
 
-
+#CSV writing testing area.... currently bunk
+import csv
+with open('output.csv', 'w') as f:
+    fieldnames = ["item#"]	
+    w = csv.writer(f)
+    for key in iitemDict:
+        w.writerow([key])
+        w.writerow(iitemDict[key])
