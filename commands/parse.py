973f970695a458d2660ab80f9515f2431ac4d49e
Tristan Brenneman <brenneman000@gmail.com>
2015-08-28 11:48:24 -0500

    working on adding more info to the parsed file

--- commands/parse.py
+++ commands/parse.py
@@ -1,12 +1,13 @@
 import sqlite3
 
-def squpdate(itemNum, supp, pcode, dcode, desc, br, bqty):
+def squpdate(itemNum, supp, pcode, dcode, desc, br, bqty, br10po, br10qty):
     conn = sqlite3.connect('purchasing.db')
     c = conn.cursor()
 
-    c.execute("CREATE TABLE IF NOT EXISTS items (itemNum INTEGER, supp TEXT, pcode TEXT, dcode TEXT, desc TEXT, br INTEGER, bqty INTEGER)")
-    c.execute("INSERT INTO items VALUES (?,?,?,?,?,?,?)",
-              (itemNum, supp, pcode, dcode, desc, br, bqty))
+    c.execute('''CREATE TABLE IF NOT EXISTS items (itemNum INTEGER, supp TEXT, pcode TEXT,
+                 dcode TEXT, desc TEXT, br INTEGER, bqty INTEGER, br10PO INTEGER, br10qty INTEGER)''')
+    c.execute("INSERT INTO items VALUES (?,?,?,?,?,?,?,?,?)",
+              (itemNum, supp, pcode, dcode, desc, br, bqty, br10po, pr10qty))
     conn.commit()
     conn.close()
     
@@ -23,6 +24,8 @@ def getItems(file):
         desc = ''
         br = ''
         bqty = ''
+        br10po = ''
+        br10qty = ''
         records = 0
 
         for line in f:
@@ -36,7 +39,12 @@ def getItems(file):
             elif line[:17].lstrip() == str(itemNum):
                 br = int(line[29:31])
                 bqty = int(line[64:])
-                squpdate(itemNum, supp, pcode, dcode, desc, br, bqty)
+                f.readline()
+                line10 = f.readline().rstrip().lstrip()
+                br10po = line10[45:50].rstrip().lstrip()
+                br10qty = line10[55:].rstrip()
+                print(line10, br10po, br10qty)
+                #squpdate(itemNum, supp, pcode, dcode, desc, br, bqty)
             elif line[:4] == 'Supp':
                 suppLine = True
                 records += 1
